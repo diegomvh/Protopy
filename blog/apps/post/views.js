ce35938c06b0c9db85962bfd73f7f10cdf149084
diego@morena <diego@morena>
2009-02-18 00:00:07 -0300

    continuan saliendo errores de toooooodos lados :)

--- blog/apps/post/views.js
+++ blog/apps/post/views.js
@@ -21,6 +21,14 @@ function add_post(title, body, tags){
     return obj;
 }
 
+function set_tags_by_title(post, tag_title){
+    tag_title = isarray(tag_title)? tag_title : [tag_title];
+    for each (var title in tag_title) {
+	var tag = Tag.objects.get({'title':title});
+	post.tags_set.add(tag);
+    }
+}
+
 function show_posts(){
     var t = loader.get_template('post.html');
     document.write(t.render(new Context({posts:Post.objects.all()})));
@@ -30,4 +38,5 @@ function show_posts(){
 $P({ 'syncdb': syncdb,
      'add_tag': add_tag,
      'add_post': add_post,
-     'show_posts': show_posts });
+     'show_posts': show_posts,
+     'set_tags_by_title': set_tags_by_title});
--- packages/doff/template/default_filters.js
+++ packages/doff/template/default_filters.js
@@ -15,9 +15,9 @@ register.filter(join);
 function slugify(value) {
     var badchars = ['á','é','í','ó','ú','ñ'];
     var goodchars = ['a','e','i','o','u','n'];
-    value = value.replace(/[^\w\s-]/g, '').strip().toLowerCase();
     for (var i = 0, length = badchars.length; i < length; i++)
         value = value.replace(badchars[i], goodchars[i], 'g');
+    value = value.replace(/[^\w\s-]/g, '').strip().toLowerCase();
     return value.replace(/[-\s]+/g, '-');
 }
 register.filter(slugify);
