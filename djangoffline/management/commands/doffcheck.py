9888c03067b34c495fc7e56688978db418febf74
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-04-08 04:37:37 -0300

    Implementando un comando básico

---
+++ djangoffline/management/commands/doffcheck.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from django.core.management.base import *
+
+
+class Command(NoArgsCommand):
+    requires_model_validation = False
+    can_import_settings = True
+    
+    def handle_norags(self, **options):
+        print "Hola mundo"
