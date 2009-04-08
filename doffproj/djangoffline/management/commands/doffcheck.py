6198529de3d7eb3a4e6cd1990ec069d152684dbf
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-04-08 05:30:11 -0300

    Quitar un pyc, acutalizar el .hgignore, urls.py

--- doffproj/djangoffline/management/commands/doffcheck.py
+++ doffproj/djangoffline/management/commands/doffcheck.py
@@ -4,8 +4,10 @@
 
 from django.core.management.base import *
 
-
 class Command(NoArgsCommand):
+    help = """
+        Probando el sistema de comandos de Django.
+    """
     requires_model_validation = False
     can_import_settings = True
     
