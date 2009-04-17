39a5f2caad2a5ca1b8ce24fc03c6dcc7cc942d6f
diego@morena <diego@morena>
2009-04-17 12:57:15 -0300

    valida que este firebug para los logs, falta cambiar todos los print del proyecto :)

--- packages/logging/handlers.js
+++ packages/logging/handlers.js
@@ -5,20 +5,30 @@ var FirebugHandler = type('FirebugHandler', [logging.Handler], {
 
     '__init__': function __init__() {
 
-        //TODO: Validar si hay firebug instalado
-
-        //Ok tengo firebug
-        this.functions[logging.CRITICAL] = window.console.error;
-        this.functions[logging.ERROR]= window.console.error;
-        this.functions[logging.WARN]= window.console.warn;
-        this.functions[logging.WARNING]= window.console.warn;
-        this.functions[logging.INFO]= window.console.info;
-        this.functions[logging.DEBUG]= window.console.debug;
+        if (window.console) {
+            //Ok tengo firebug
+            this.functions[logging.CRITICAL] = window.console.error;
+            this.functions[logging.ERROR]= window.console.error;
+            this.functions[logging.WARN]= window.console.warn;
+            this.functions[logging.WARNING]= window.console.warn;
+            this.functions[logging.INFO]= window.console.info;
+            this.functions[logging.DEBUG]= window.console.debug;
+            this.trace = window.console.trace;
+        } else {
+            alert('Firebug is not installed, please install from: ')
+            this.functions[logging.CRITICAL] = function(){};
+            this.functions[logging.ERROR]= function(){};
+            this.functions[logging.WARN]= function(){};
+            this.functions[logging.WARNING]= function(){};
+            this.functions[logging.INFO]= function(){};
+            this.functions[logging.DEBUG]= function(){};
+            this.trace = function(){};
+        }
     },
 
     emit: function(record) {
         if (logging.ERROR == record.levelno)
-            window.console.trace();
+            this.trace();
         this.functions[record.levelno](this.format(record));
     }
 });
