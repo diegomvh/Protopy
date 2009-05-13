48e705561fa357ba2b5e4380e219c80f85907791
dvanhaaster@inf-dvh <dvanhaaster@inf-dvh>
2009-05-13 13:54:14 -0300

    agrego estilos de barra tipo facebook gracias a pablo

--- packages/doff/utils/toolbar.js
+++ packages/doff/utils/toolbar.js
@@ -1,30 +1,50 @@
+require('sys');
+require('event');
+
 var Toolbar = type('Toolbar', object, {
 
     __init__: function() {
-	this.bar = document.createElement('ul');
-	this.bar.id = 'toolbar';
+        this.content = document.createElement('div');
+        this.content.id = 'toolbar';
+        this.ul = document.createElement('ul');
+        this.content.insert(this.ul);
+        this.stylesheet = document.createElement('link');
+        this.stylesheet.rel = "stylesheet";
+        this.stylesheet.type = "text/css";
+        this.stylesheet.href = sys.module_url('doff.utils', 'resources/toolbar.css');
     },
 
     add_item: function(name) {
-	var item = document.createElement('li');
-	item.update(name);
-	this.bar.insert(item);
-	return item;
+        var item = document.createElement('li');
+        item.update(name);
+        this.ul.insert(item);
+        return item;
+    },
+
+    add_panel: function(name, content) {
+        var item = this.add_item(name);
+        var panel = document.createElement('div');
+        panel.insert(content);
+        panel.hide();
+        this.content.insert(panel);
+        event.connect(item, 'click', function() { panel.toggle(); });
+        return panel;
     },
 
     hide: function() {
-	this.bar.style.display = 'none';
+	this.ul.style.display = 'none';
     },
 
     show: function() {
 	if (!this.displayed)
 	    this._display();
-	this.bar.style.display = '';
+	this.ul.style.display = '';
     },
     
     _display: function () {
 	var body = $$('body')[0];
-	body.insert(this.bar);
+        body.insert(this.stylesheet);
+	body.insert(this.content);
 	this.displayed = true;
     }
     
