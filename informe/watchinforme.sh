72bf0eb1314a567d90aa623df1ccaae997ba556c
diegomvh <devnull@localhost>
2009-06-01 21:59:15 -0300

    protopy

--- informe/watchinforme.sh
+++ informe/watchinforme.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-NOMBRE="informe.tex desarrollo/protopy.tex"
+NOMBRE="informe.tex apendices/protopy.tex apendices/doff.tex"
 
 while  inotifywait -e modify $NOMBRE ; do
     yes R | pdflatex $NOMBRE 
