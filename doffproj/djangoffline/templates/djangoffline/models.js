56f411e0f30f7eb13ad44caaf630e5684bc1edf6
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-06-17 12:29:24 -0300

    Limpieza del template

--- doffproj/djangoffline/templates/djangoffline/models.js
+++ doffproj/djangoffline/templates/djangoffline/models.js
@@ -1,29 +1,4 @@
-{% comment %}
-var models = require('doff.db.models.base');
-
-var Tag = type('Tag', models.Model, {
-    slug: new models.SlugField('Slug', {'help_text':'Automatically built from the title.', 'primary_key':true, 'editable': false}),
-    title: new models.CharField('Title', {'max_length':30})
-});
-
-var Post = type('Post', models.Model, {
-    slug: new models.SlugField('Slug', {'primary_key':true, 'editable': false}),
-    title: new models.CharField('Title', {'max_length':30}),
-    tags: new models.ManyToManyField(Tag),
-    date: new models.DateTimeField('Date', {'editable': false, 'auto_now': true}),
-    body: new models.TextField('Body Text'),
-    Meta: {
-        ordering: ['-date']
-    }
-});
-
-publish({
-    Tag: Tag,
-    Post: Post
-});
-
-{% endcomment %} {% load export_models %}
-{% spaceless %}
+{% load export_models %}{% spaceless %}
 var models = require('doff.db.models.base');
 {% for model_name, fields in models.iteritems %}
 var {{ model_name }} = type("{{ model_name }}", models.Model, {
@@ -34,8 +9,6 @@ var {{ model_name }} = type("{{ model_name }}", models.Model, {
 {% endfor %}
 
 publish({
-{% for name, _ in models.iteritems %}
-	{{ name }}: {{ name }}{% if not forloop.last %},{% endif %}
+{% for name, _ in models.iteritems %} {{ name }}: {{ name }}{% if not forloop.last %},{% endif %}
 {% endfor %}
-});
-{% endspaceless %}
+});{% endspaceless %}
