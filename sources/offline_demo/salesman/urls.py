6d109b9954a71ca7eb2b53654f211461202f5ce7
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-09-25 13:53:04 -0300

    Mejorando el uso de vista generica

--- sources/offline_demo/salesman/urls.py
+++ sources/offline_demo/salesman/urls.py
@@ -6,16 +6,12 @@ urlpatterns = patterns('',
         # Ver una ciudad
         (r'ciudad/(?P<ciudad_id>\d{1,5})/$', view_ciudad),
         # Editar una ciudad
-        (r'ciudad/(?P<ciudad_id>\d{1,5})/edit/$', 'django.views.generic.create_update.update_object',{
+        (r'ciudad/(?P<object_id>\d{1,5})/edit/$', 'django.views.generic.create_update.update_object',{
             'model': Ciudad,
-            'object_id': None,
-         
+            'post_save_redirect': '/salesman/ciudades/',
+            'extra_context': {'title': 'Editar ciudad', 'submit_text': 'Actualizar',}, 
         }),
-        # Agrear ciudad
-        #(r'ciudad/add/$', add_edit_ciudad),
-        # Listado de ciudades
-        #(r'ciudades/$', list_ciudad),
-        # (r'ciudades/(?P<per_page>\d{1,5})/(?P<start_page>\d{1,5})/$', list_ciudad),
+        
         (r'ciudades/$', 'django.views.generic.list_detail.object_list',{
             'queryset': Ciudad.objects.all()
             }
