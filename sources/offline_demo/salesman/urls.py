a23d3cc7890c4b59739ebb51a556a986d4a8f9c4
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-10-11 17:14:38 -0300

    Reagregar protopy.js, no se por que estaba deshabilitado

--- sources/offline_demo/salesman/urls.py
+++ sources/offline_demo/salesman/urls.py
@@ -225,4 +225,41 @@ urlpatterns = patterns('',
                                                                                
         }),
         
+        #---------------------------------------------------------------------------------
+        # Pedido
+        #---------------------------------------------------------------------------------
+        (r'pedido/$', 'django.views.generic.simple.redirect_to', {'url':'/salesman/pedidos/'}),
+        
+        # vendedors -> Edit
+        (r'pedido/(?P<object_id>\d{1,5})/edit/$', 'django.views.generic.create_update.update_object',{
+            'model': Pedido,
+            'post_save_redirect': '/salesman/pedidos/',
+            'template_name': 'forms.html',
+            'extra_context': {'title': 'Editar pedido', 'submit_text': 'Actualizar',}, 
+        }),
+        
+        # pedido -> Listar
+        (r'pedidos/$', 'django.views.generic.list_detail.object_list',{
+            'queryset': Pedido.objects.all()
+            }
+        ),
+        
+        # pedido -> Crear
+        (r'pedidos/add/$', 'django.views.generic.simple.redirect_to', 
+            {'url': '../../pedido/add/'}),
+            
+        (r'pedido/add/$', 'django.views.generic.create_update.create_object', {
+            'model': Pedido,
+            'template_name': 'forms.html',
+            'post_save_redirect': '/salesman/pedidos/',
+                                                                               
+        }),
+        # pedido -> Eliminar
+        (r'pedido/(?P<object_id>\d{1,5})/delete/$', 'django.views.generic.create_update.delete_object', {
+            'model': Pedido,
+            'post_delete_redirect': '/salesman/pedidos/',
+            'template_name': 'confirm_delete.html',
+                                                                               
+        }),
+        
 )
