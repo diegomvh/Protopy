c6d361d85fdf78085a49e9ee6a49fdcba09f5e39
nahuel@asgard-jaunty <nahuel@asgard-jaunty>
2009-10-19 11:34:00 -0300

    Comenzando con el migrate_app

--- sources/offline/management/commands/migrate_app.py
+++ sources/offline/management/commands/migrate_app.py
@@ -1,39 +1,78 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+
+ migrate_app app
+ La app debe estar en {{ settings.INSTALLED_APPS }}
+ Creamos
+ * crear {{ APP_PATH }}/remote_models.py
+ * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/models.js
+ * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/views.js
+ * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/urls.js
 
 
-# translate_app app
-# app debe estar en {{ settings.INSTALLED_APPS }}
-# Creamos
-# * crear {{ APP_PATH }}/remote_models.py
-# * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/models.js
-# * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/views.js
-# * crear {{ OFFLINE_ROOT}}/{{APP_NAME}}/urls.js
-
-
-
+"""
 from os.path import dirname, abspath, exists, join
 from django.db.models.loading import get_app
 from django.template import Template
 from django.template.context import Context
-from djangoffline.management.commands import get_template_colision
+from offline.management.commands import get_template_colision
+from django.core.exceptions import ImproperlyConfigured
 import os, glob
 from os.path import basename
+import sys
+from django.core.management.base import BaseCommand, make_option, CommandError
 
-from django.core.management.base import *
-
-class Command(AppCommand):
+class Command(BaseCommand):
     help = """
-        Creates the skeleton for an a offline app in OFFLINE_ROOT.
+        Creates the skeleton for an a offline app in the remote site.
     """
     
     option_list = (
                     make_option('-f', '--force', action='store_true', default = False),
                     
-    ) + AppCommand.option_list
+    ) + BaseCommand.option_list
     
     requires_model_validation = True
     can_import_settings = True
+    
+    
+    def handle(self, *remote_and_apps, **largs):
+        from django.db import models
+        from django.conf import settings
+        # Remotesite
+        mod = __import__(settings.ROOT_URLCONF)
+        from offline.sites import REMOTE_SITES
+        
+        if not REMOTE_SITES:
+            raise CommandError("No remote sites registered at ROOT_URLCONF.")
+        
+        if not remote_and_apps:
+            raise CommandError("Remote site missing")
+        
+        remote_site_name = remote_and_apps[0]
+        
+        
+        if remote_site_name in REMOTE_SITES:
+            remote_site = REMOTE_SITES[remote_site_name]
+        else:
+            raise CommandError("Remote Site named '%s' does not exist. There are %d" % 
+                                (remote_site_name, len(REMOTE_SITES)) +
+                               " remote sites registered: %s" % ','.join(REMOTE_SITES.keys()))
+        
+        app_labels =  remote_and_apps[1:]
+        
+        try:
+            app_list = [models.get_app(app_label) for app_label in app_labels]
+        except (ImproperlyConfigured, ImportError), e:
+            raise CommandError("%s. Are you sure your INSTALLED_APPS setting is correct?" % e)
+        
+        for app in app_list:
+            pass
+        
+        
+        print remote_site_name, app_labels
+    
     
     def handle_app(self, app, **options):
         from django.conf import settings
