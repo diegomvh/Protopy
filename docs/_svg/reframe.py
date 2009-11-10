ad0ac33df6ca4aacfe0faec8caac00939f88bcba
nahuel@asgard-karmic <nahuel@asgard-karmic>
2009-11-10 15:07:12 -0300

    Correcciones en el captítulo de RemoteSite y agregado al capitulo de la Demo

--- docs/_svg/reframe.py
+++ docs/_svg/reframe.py
@@ -1,36 +1,60 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Redimencionar imágenes utilizando PIL para que su tamaño no rompa la estructura de la hoja.
 
-from PIL import Image
+'''
+Redimencionar imágenes utilizando PIL para que su tamaño no rompa 
+la estructura de la hoja en Sphinx/LaTeX
+'''
+
 import sys
+try:
+    from PIL import Image
+except ImportError:
+    print "Es necesario instalar Python Imaging Library (PIL)"
+    sys.exit(3)
 
+class NoNeedReframeException(Exception):
+    pass
+
+# Color de fondo
 FONDO = (255, 255, 255)
 
 def reframe_fname(name):
+    '''
+    Redimenciona una imagen, guardandola en 
+    @param name: Nombre de un archivo
+    '''
     try:
         img = Image.open(name).convert('RGB')
-        w, h = img.size
-        if h > w:
-            n_w = h
+        ancho, alto = img.size
+        if alto > ancho and float(alto)/ancho > 1.2: # Relación máxima 1/4
+            nuevo_ancho = alto
         else:
-            n_w = w
-            
-        new_img = Image.new('RGB', (n_w, h), FONDO )
+            # No se necesita redimencionar
+            raise NoNeedReframeException()
         
-        ext = name[name.rindex('.')+1:]
-        name =  name[:name.rindex('.')]
-        new_name = '%s_1.%s' % (name, ext, )
+        # Nueva imagen en RGB    
+        new_img = Image.new('RGB', (nuevo_ancho, alto), FONDO )
         
-        print type(new_img), new_img.__class__
-        new_img.paste(img, ((n_w - w) / 2 ,0))
+        #ext = name[name.rindex('.')+1:]
+        #name =  name[:name.rindex('.')]
+        #new_name = '%s_1.%s' % (name, ext, )
         
-        new_img.save(new_name)
+        new_img.paste(img, ((nuevo_ancho - ancho) / 2 ,0))
+        
+        new_img.save(name)
     except IOError, e:
-        print "No se puede abrir '%s'" % name
+        print "No se puede abrir '%s. Formato inválido? %s'" % (name, e)
+    except NoNeedReframeException:
+        pass
     else:
-        print name, w, h
+        print "Se redimencionó %s (%d, %d) -> (%d, %d)" % (name,
+                                                           ancho,
+                                                           alto,
+                                                           nuevo_ancho,
+                                                           alto
+                                                           ) 
 
 def main(argv = sys.argv):
     names = argv[1:]
