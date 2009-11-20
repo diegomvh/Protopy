335e90006e52e0efa3f25893eccb44c3d0742564
diego van Haaster <diegomvh@gmail.com>
2009-11-20 20:18:59 -0300

    armoando pedidos

--- sources/salesman/apps/ventas/views.py
+++ sources/salesman/apps/ventas/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render_to_response
 from salesman.apps.ventas.models import Pedido
 from salesman.apps.core.models import Producto
@@ -45,14 +45,49 @@ def edit_pedido(request, object_id):
     
 def agregar_producto(request, producto):
     if 'pedido' not in request.session:
-        pedido = {'cliente': None, 'items': {}}
+        pedido = {'cliente': None, 'items': {}, 'subtotal': 0.0}
     else:
         pedido = request.session['pedido']
 
-	producto = get_object_or_404(Producto, id = producto)
+    producto = get_object_or_404(Producto, id = producto)
+    
     if not pedido['items'].has_key(producto.id):
         pedido['items'][producto.id] = {'cantidad': 0, 'producto': producto, 'importe': 0}
     pedido['items'][producto.id]['cantidad'] += 1
     pedido['items'][producto.id]['importe'] = pedido['items'][producto.id]['cantidad'] * producto.precio
+    pedido['subtotal'] += float(pedido['items'][producto.id]['importe'])
     request.session['pedido'] = pedido
     return render_to_response('pedido.html', {'pedido': pedido, 'producto': producto}, context_instance = RequestContext(request))
+
+def modificar_pedido(request):
+    if request.method != 'POST' or 'pedido' not in request.session:
+        raise Http404()
+    pedido = request.session['pedido']
+    
+    subtotal = 0.0
+    items = []
+    for id, item in pedido['items'].iteritems():
+        try:
+            cantidad = int(request.REQUEST['cantidad_%s' % id])
+        except ValueError:
+            continue
+        if request.REQUEST.has_key('quitar_%s' % id) or cantidad < 0:
+            continue
+        else:
+            item['cantidad'] = cantidad
+            item['importe'] = cantidad * item['producto'].precio
+            subtotal += float(item['importe'])
+        items.append(item)
+
+    pedido['items'] = dict(map(lambda item: (item['producto'].id, item), items))
+    pedido['subtotal'] = subtotal
+    
+    request.session['pedido'] = pedido
+    return ver_pedido(request)
+
+def ver_pedido(request):
+    if 'pedido' not in request.session or len(request.session['pedido']['items']) == 0:
+        return render_to_response('pedido.html', {}, context_instance = RequestContext(request))
+    pedido = request.session['pedido']
+    return render_to_response('pedido.html', {'pedido': pedido}, context_instance = RequestContext(request))
+
