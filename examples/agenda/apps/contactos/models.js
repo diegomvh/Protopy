9caf8251c712b9275c2b6db39f4768cb118de508
dvanhaaster@inf-dvh <dvanhaaster@inf-dvh>
2009-12-07 13:48:58 -0300

    errores

--- examples/agenda/apps/contactos/models.js
+++ examples/agenda/apps/contactos/models.js
@@ -1,13 +1,13 @@
 var models = require('doff.db.models.base');
 
-var Contacto = type('Contacto', [models.Model], {
-	nombre = models.CharField({max_length: 50}),
-	apellido = models.CharField({max_length: 50}),
-	correo = models.EmailField({max_length: 50}),
-	telefono = models.CharField({max_length: 50}),
+var Contacto = type('Contacto', [ models.Model ], {
+	nombre: new models.CharField({max_length: 50}),
+	apellido: new models.CharField({max_length: 50}),
+	correo: new models.EmailField({max_length: 50}),
+	telefono: new models.CharField({max_length: 50}),
 	
 	__str__: function() {
-		return "%s, %s".subs(this.apellido, this.nombra);
+		return "%s, %s".subs(this.apellido, this.nombre);
 	}
 });
 
