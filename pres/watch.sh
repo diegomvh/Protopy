01ccdf596743c8d53e83907c61e8561385cb5124
nahuel@asgard-karmik <nahuel@asgard-karmik>
2009-12-13 20:54:49 -0300

    Beamer

--- pres/watch.sh
+++ pres/watch.sh
@@ -2,5 +2,14 @@
 
 # Este es el script para compilar automáticamente
 # mediante el makefile
+INOTIFYWAIT="inotifywait"
+
+
+if ! which $INOTIFYWAIT 1>/dev/null; then
+    echo "No encuentro $INOTIFYWAIT"
+    echo "Por favor instalelo desde inotifytools".
+    exit
+fi
+
 
 
